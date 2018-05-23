01ad03571db27f13d81e516a5d6f1f702c989405
Caitlin Elfring <caitlin@graciouseloise.com>
2018-05-23 09:55:35 -0400

    Add comments for godoc

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
-// Based on https://gist.github.com/buckhx/45a54e75f7a9484ca9d69f699b929eca
+/*
+nist-password-validator will detect passwords that do not meet the following requirements:
 
+1. Minimum of 8 characters
+1. Maximum of 64 characters
+1. Only contain ASCII characters
+1. Not be a common password based on a supplied common password file
+
+The program will take a list of newline-delimited passwords from STDIN.
+It will check each of these passwords against the above criteria and output any passwords that fail
+to meet the criteria along with the failure reason.
+
+A filename which contains the list of newline-delimited common passwords should be supplied
+as the first parameter of the program.
+
+	# Run with a single password
+	echo "MyPassword" | nist-password-validator myCommonPasswordList.txt
+
+	# Run with a file of passwords
+	cat "myPasswordFile.txt" | nist-password-validator myCommonPasswordList.txt
+
+Based on https://gist.github.com/buckhx/45a54e75f7a9484ca9d69f699b929eca
+*/
 package main
 
 import (
