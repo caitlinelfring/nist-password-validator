cfe638ac545cd0f7ba4d1fcd349d6e3d5f7f595e
Caitlin Elfring <caitlin@graciouseloise.com>
2018-06-01 15:47:54 -0400

    Add tests for validator max/min characters, isCommon methods

--- password/validator_test.go
+++ password/validator_test.go
@@ -5,6 +5,65 @@ import (
 	"testing"
 )
 
+func TestValidator(t *testing.T) {
+	var testStrings = []struct {
+		pass               string
+		overMaxCharacters  bool
+		underMinCharacters bool
+		isCommon           bool
+	}{
+		{"", false, true, false},
+		{"abcd", false, true, false},
+		{"qwertyui", false, false, false},
+		{"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopas", false, false, false},
+		{"asdlfdja;sfjajfpojp8jp8efpw3jpowefjohefpowahepfo", false, false, false},
+		{"asdlfdja;sfjajfpojp8jp8efpw3jpowsa efjohefpowahepfo", false, false, false},
+		{"asdlfdja;sfjajfpojp8jp8efpw3jpowsa efjohefpowahepfo", false, false, false},
+		{"asdlfdja;lsdijfa;sdfpoaisdufpoaisuf9oarpfauhrfiuehfpiudhfioufgoiudfhgoiudfgpdupodsifpuosiUFPAOSIDUFPAOSDIUFP", true, false, false},
+		{"☺☻☹", false, false, false},
+		{"日a本b語ç日ð本Ê語þ日¥本¼語i日©", false, false, false},
+		{"日a本b語ç日ð本Ê語þ日¥本¼語i日©日a本b語ç日ð本Ê語þ", true, false, false},
+		{"\x80\x80\x80\x80asdasdaslkjdalskjf", false, false, false},
+		{"ಠ_ಠasdsadಠ_ಠ", false, false, false},
+		{"zzzzfitt", false, false, true},
+		{"vjht008", false, true, true},
+		{"123456789", false, false, true},
+	}
+
+	filename := "password_list_test.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// TODO: Add more Validator tests
+	// acceptASCIIOnly = false
+	// min > max
+	// max = min = 0 (defaults?)
+	validator := NewValidator(true, 8, 64)
+	err = validator.AddCommonPasswords(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, test := range testStrings {
+		pass := Password(test.pass)
+		isCommon := validator.isCommon(pass)
+		if isCommon != test.isCommon {
+			t.Errorf("%s (isCommon) -- expected: %v, got: %v", test.pass, test.isCommon, isCommon)
+		}
+
+		overMaxCharacters := validator.overMaxCharacters(pass)
+		if overMaxCharacters != test.overMaxCharacters {
+			t.Errorf("%s (overMaxCharacters) -- expected: %v, got: %v", test.pass, test.overMaxCharacters, overMaxCharacters)
+		}
+
+		underMinCharacters := validator.underMinCharacters(pass)
+		if underMinCharacters != test.underMinCharacters {
+			t.Errorf("%s (underMinCharacters) -- expected: %v, got: %v", test.pass, test.underMinCharacters, underMinCharacters)
+		}
+	}
+}
+
 func TestValidatorValidatePassword(t *testing.T) {
 	var testStrings = []struct {
 		value    string
@@ -33,10 +92,6 @@ func TestValidatorValidatePassword(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// TODO: Add more Validator tests
-	// acceptASCIIOnly = false
-	// min > max
-	// max = min = 0 (defaults?)
 	validator := NewValidator(true, 8, 64)
 
 	// Creating a new Validator and adding the common password list are two different steps,
